99b091275efd7e467b0a99b6f44ea8b101903327
mr-chamika <chamikauni2001@gmail.com>
2025-08-21 17:27:31 +0530

    refactor: restructure authentication layout and remove unused OTP verification component

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -8,7 +8,8 @@ export default function RootLayout() {
       <StatusBar barStyle="dark-content" backgroundColor="#F2F0EF" />
 
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="views" options={{ headerShown: false }} />
       </Stack>
